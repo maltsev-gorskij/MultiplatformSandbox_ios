d1e4583000efa8a6640d404a35b4e36c57f99a3b
lapin <alapin@hashinc.tech>
2023-02-14 17:07:00 +0300

    add Testing and update detail vm for launch detail

--- iosApp/Presentation/Screens/LaunchDetail/ViewModel/LaunchDetailViewModel.swift
+++ iosApp/Presentation/Screens/LaunchDetail/ViewModel/LaunchDetailViewModel.swift
@@ -6,34 +6,35 @@
 //  Copyright © 2023 orgName. All rights reserved.
 //
 
-import Foundation
 import shared
+import sharedSwift
 
 final class LaunchDetailViewModel: ObservableObject {
   @Published private(set) var rocketLaunch: RocketLaunch?
   @Published private(set) var error: String?
-
+  
   private let interactor: LaunchesInteractor
-
+  
   init(interactor: LaunchesInteractor, launchId: String) {
     self.interactor = interactor
     getLaunch(id: launchId)
   }
-
+  
   private func getLaunch(id: String) {
-//    interactor.getLaunchById(launchId: id) { [weak self] rocketLaunch, error in
-//      guard let self = self else { return }
-//      if let error = error {
-//        print("DEBUG: get launch by id: \(id) with error: \(error.localizedDescription)")
-//        DispatchQueue.main.async {
-//          self.error = error.localizedDescription
-//        }
-//        return
-//      }
-//      guard let rocketLaunch = rocketLaunch else { return }
-//      DispatchQueue.main.async {
-//        self.rocketLaunch = rocketLaunch
-//      }
-//    }
+    interactor.getLaunchById(launchId: id) { [weak self] sharedResult, _ in
+      guard let self = self, let sharedResult = sharedResult else { return }
+      let sharedResultKs = SharedResultKs(sharedResult)
+      DispatchQueue.main.async {
+        switch sharedResultKs {
+        case .exception(let sharedResultException):
+          guard let dbException = sharedResultException.exception as? DatabaseExceptions else { return }
+          let errorMessage = DatabaseExceptionsKs(dbException).sealed.errorMessage
+          print("DEBUG: error - \(errorMessage)")
+          self.error = errorMessage
+        case .success(let sharedResultSuccess):
+          self.rocketLaunch = sharedResultSuccess.data
+        }
+      }
+    }
   }
 }
---
+++ iosApp/Testing/ValidationInteractor.swift
@@ -0,0 +1,67 @@
+//
+//  ValidationInteractor.swift
+//  iosApp
+//
+//  Created by Андрей Лапин on 14.02.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import shared
+import sharedSwift
+
+fileprivate class Testing {
+  let validation = ValidationInteractor()
+
+  func testing() {
+    validation.getSuspendNetworkSuccess { sharedResult, _ in
+      guard let sharedResult = sharedResult else { return }
+      let sharedKs = SharedResultKs(sharedResult)
+      switch sharedKs {
+      case .success(let success):
+        print("DEBUG: \(success)")
+      case .exception(let failure):
+        print("DEBUG: error \(failure.exception)")
+      }
+    }
+    validation.getSuspendNetworkException { sharedResult, _ in
+      guard let sharedResult = sharedResult else { return }
+      let sharedKs = SharedResultKs(sharedResult)
+      switch sharedKs {
+      case .success:
+        break
+      case .exception(let failure):
+        let exception = KtorExceptionsKs(failure.exception as! KtorExceptions)
+        print("DEBUG: error -", exception.sealed.errorCode)
+      }
+    }
+    
+    validation.getSuspendSuccess { sharedResult, _ in
+      guard let sharedResult = sharedResult else { return }
+      let sharedResultKs = SharedResultKs(sharedResult)
+      sharedResult.onSuccess { result in
+        guard let str = result as? String else { return }
+      }
+    }
+    
+    validation.getSuspendError { sharedResult, _ in
+      guard let sharedResult = sharedResult else { return }
+      let sharedResultKs = SharedResultKs(sharedResult)
+      switch sharedResultKs {
+      case .exception(let sharedResultException):
+        print("DEBUG: with error -", sharedResultException.exception.message ?? "")
+      case .success:
+        break
+      }
+    }
+    
+    let sharedResult = validation.getNonSuspendDatabaseEntries()
+    let sharedResultKs = SharedResultKs(sharedResult)
+    switch sharedResultKs {
+    case .exception(let sharedResultException):
+      let exception = DatabaseExceptionsKs(sharedResultException.exception as! DatabaseExceptions)
+      print("DEBUG: with error -", exception.sealed)
+    case .success:
+      break
+    }
+  }
+}
